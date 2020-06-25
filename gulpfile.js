92e765e65bdc36cfda0307f6ada246a4f27378b3
Artem Bazhin <bazhin.artyom@yandex.ru>
2020-06-25 22:27:29 +0300

    Add css utils

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 
-function build() {
+function ui() {
   return src('src/main.scss')
     .pipe(sass())
     .pipe(postcss())
     .pipe(rename('selectel-ui.css'))
     .pipe(dest('dist'));
 }
+
+function utils() {
+  return src('src/common/scss/utils.scss')
+    .pipe(sass())
+    .pipe(postcss())
+    .pipe(rename('utils.css'))
+    .pipe(dest('dist'));
+}
+
+const build = parallel(ui, utils);
 
 function dev() {
   return watch('src/**/*.scss', build);
